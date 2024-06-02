1efdad3e5dafa782c668247022e667b9fc004090
welttowelt <oli@starkware.co>
2024-06-02 13:45:59 +0200

    Install OpenZeppelin contracts and update MyToken.sol

--- defi-contracts/src/MyToken.sol
+++ defi-contracts/src/MyToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
